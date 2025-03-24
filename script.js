cfe301cb2763dc326b66c5597ec9a1cedf3a05cf
makeprodigy <payneparihar13@gmail.com>
2025-03-24 20:41:22 +0530

    improved the code

--- script.js
+++ script.js
@@ -1,38 +1,31 @@
-const todoList = [{
-
-}];
+const todoList = [];
 
 
 function renderTodoList() {
-
   let todoListHTML = '';
 
   todoList.forEach((todoObject, index) => {
     const { name, dueDate } = todoObject;
 
     const html = `
-      <p>"${name}" ${dueDate}
-        <button
-        class="js-delete-todo-button" >Delete
-        </button>
-      </p>
+      <li>
+        "${name}" - ${dueDate}
+        <button class="js-delete-todo-button" data-index="${index}">Delete</button>
+      </li>
     `;
-    todoListHTML += html
-  })
+    todoListHTML += html;
+  });
 
-  console.log(todoListHTML)
-
-  document.querySelector('.js-todo-list')
-    .innerHTML = todoListHTML;
+  document.querySelector('.js-todo-list').innerHTML = `<ul>${todoListHTML}</ul>`;
 
   document.querySelectorAll('.js-delete-todo-button')
-    .forEach((deleteButton, index) => {
-      deleteButton.addEventListener('click', () => {
-        console.log(index);
-        todoList.splice(index,1);
+    .forEach((deleteButton) => {
+      deleteButton.addEventListener('click', (event) => {
+        const index = event.target.dataset.index;
+        todoList.splice(index, 1);
         renderTodoList();
-      })
-    })
+      });
+    });
 }
 
 document.querySelector('.js-add-todo-button')
@@ -40,20 +33,27 @@ document.querySelector('.js-add-todo-button')
     addTodo();
   });
 
-function addTodo() {
-  const inputElement = document.querySelector('.js-name-input');
-  const name = inputElement.ariaValueMax;
-
-  const dateInputElement = document.querySelector('.js-date-input');
-  const dueDate = dateInputElement.value
-
-  todoList.push({
-    name,
-    dueDate,
-  });
-
-  console.log(todoList)
-
-  inputElement.value = '';
-  renderTodoList();
-}
+  function addTodo() {
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value.trim(); // Ensure it's not empty
+  
+    const dateInputElement = document.querySelector('.js-date-input');
+    const dueDate = dateInputElement.value.trim();
+  
+    if (!name) {
+      alert("Please enter a valid todo name.");
+      return;
+    }
+  
+    if (!dueDate) {
+      alert("Please select a valid due date.");
+      return;
+    }
+  
+    todoList.push({ name, dueDate });
+  
+    inputElement.value = '';  // Clear input
+    dateInputElement.value = '';
+  
+    renderTodoList();
+  }
